62b83f44998c2216d89e8b3fd83c994e1f08b15c
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-07-14 23:55:24 +0530

    started menu part

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react' // eslint-disable-line no-unused-vars
+import './ExploreMenu.css'
+const ExploreMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExploreMenu
