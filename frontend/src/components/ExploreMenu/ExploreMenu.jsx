14414a4c87f1c775cd4c52bac7be00cd9e3c8151
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-07-15 23:56:51 +0530

    added explore menu list

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,23 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets' 
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delecturing array of dishes,Our mission is to </p>
+      <div className="div explore-menu-list">
+        {menu_list.map(()=>{
+          return(
+            <div key={index} className='explore-menu-list-item'>
+              <img src={item.menu_image} alt=""></img>
+              <p>{items.menu_name}</p>
+            </div>
+          )
+            
+          
+        })}
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 const Home = () => {
   return (
     <div>
      <Header />
+     <ExploreMenu/>
     </div>
   )
 }
