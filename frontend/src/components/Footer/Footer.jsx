1e1bcb19c257a825131e1b0591ebbae1b8f90722
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-11 01:36:36 +0530

    Added footer part

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';  // eslint-disable-line no-unused-vars
+import './Footer.css';
+import { assets } from '../../assets/assets';
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="Logo" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, maxime? Ipsa reprehenderit nam facilis rem, ducimus dicta nostrum mollitia quasi autem laboriosam deserunt non! Molestias iusto vero illum ullam repellendus.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
+        </div>
+      
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91 567-876-78968</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 © tomato.com - All Rights Reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
