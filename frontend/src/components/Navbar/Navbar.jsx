6f019709ad76bb944ff27c460c8f3bbb7c6eea36
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-12 22:22:26 +0530

    Added AppDownload part.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
-import React,{useState} from 'react' // eslint-disable-line no-unused-vars
-import './Navbar.css'
-import { assets } from '../../assets/assets'  // eslint-disable-line no-unused-vars
+import React, { useState } from 'react'; // eslint-disable-line no-unused-vars
+import './Navbar.css';
+import { assets } from '../../assets/assets'; // eslint-disable-line no-unused-vars
+
 const Navbar = () => {
-    const [menu,setMenu] = useState("menu"); // eslint-disable-line no-unused-vars
-    
-  return (
-    <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo"/>
-        <ul className="navbar-menu">
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home </li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-            <li onClick={() =>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt=""></img>
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt=""></img>
-                <div className="dot"></div>
+    const [menu, setMenu] = useState("menu"); // eslint-disable-line no-unused-vars
+
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="Logo" className="logo"/>
+            <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="Search Icon" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="Basket Icon" />
+                    <div className="dot"></div>
+                </div>
+                <button className="navbar-button">Sign In</button>
             </div>
-            <button>Sign In</button>
         </div>
-        </div>
-        
-  )
+    );
 }
 
-export default Navbar
+export default Navbar;
