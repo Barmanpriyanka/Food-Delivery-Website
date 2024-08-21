26c607cf469a925b2053468bf5e6e327071417eb
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-21 23:29:36 +0530

    api endpoints to add and remove items

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
-const  foodSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    description:{type:String,rquired:true},
-    price:{type:Number,rquired:true},
-    image:{type:String,rquired:true},
-    category:{type,required:true}
-})
-const foodModel = mongoose.models.food || mongoose.model("food",foodSchema);
+const foodSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true }  // Fixed type here
+});
+
+const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
 
 export default foodModel;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from  "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 //app config
 const app=express()
 const port=4000
@@ -12,7 +13,8 @@ app.use(cors())
 connectDB();
 
 //api endpoints
-app.use("/api/food",food)
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
 
 app.get("/",(req,res)=>{
     res.send("API Working")
