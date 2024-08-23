ad9c13c4e0ae432dc4f7efb74caf15edd9ab92f9
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-08-23 19:31:58 +0530

    added List.jsx in admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,11 +1,75 @@
-import React from 'react'// eslint-disable-line no-unused-vars
-import './List.css'
-const List = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React, { useEffect, useState } from 'react'; // eslint-disable-line no-unused-vars
+import './List.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-export default List
+const List = () => {
+  const url = 'http://localhost:4000';
+  const [list, setList] = useState([]);
+
+  
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error('Error fetching the list');
+      }
+    } catch (error) {
+      toast.error('An error occurred while fetching the list');
+      console.error('Fetch list error:', error);
+    }
+  };
+
+ 
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, { id: foodId });
+      await fetchList();
+      if (response.data.success) {
+         toast.success(response.data.message)
+
+      } else {
+        toast.error('Error removing the food item');
+      }
+    } catch (error) {
+      toast.error('An error occurred while removing the food item');
+      console.error('Remove food error:', error);
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          const imageUrl = `${url}/images/${item.image}`; // Ensure this path is correct
+          return (
+            <div key={index} className='list-table-format'>
+              <img src={imageUrl} alt={item.name} />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>X</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
